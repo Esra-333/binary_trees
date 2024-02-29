2cf3500ec52972851246dc152d5a597e6fab0c1f
Esra-333 <esra.omer444@gmail.com>
2024-02-29 00:19:09 -0800

    a

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,6 +3,7 @@
  * binary_tree_node - create
  * binary_tree_node: node
  * @value: value
+ * @parent: apointer
  * Return: new node
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
